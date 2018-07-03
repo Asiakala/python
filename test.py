37e9c8bfa06169f2aaa2c5895a4b6ebadbd9a164
Asiakala <1521959381@qq.com>
2018-07-03 22:42:42 +0800

    提交函数练习文件

--- test.py
+++ test.py
@@ -1,2 +1,42 @@
 #!/usr/bin/python3
-print(7+1)
+import sys
+
+#普通函数
+def feibonaci(n):
+    a, b, counter = 0, 1, 0
+    while (counter < n):
+        yield a
+        a, b = b, a+b
+        counter += 1
+
+#带默认参数的函数
+def printme(x, y = 3):
+    print(x,y)
+    return x+y
+
+#匿名函数
+sum = lambda arg1, arg2: arg1+arg2
+
+#参数不定函数，以元组形式传入
+def printinfo(x, *argv):
+    print(x)
+    print(argv)
+
+#参数不定函数，以字典形式传入
+def printinfodict(x, **argv):
+    print(x)
+    print(argv)
+
+f = feibonaci(10)
+
+printme(y=1, x=2)
+print(printme(2))
+printinfo(1,2,3)
+printinfodict(1,a=2, b=3)
+print(sum(9, 10))
+
+while True:
+    try:
+        print(next(f), end = " ")
+    except StopAsyncIteration:
+        sys.exit()
